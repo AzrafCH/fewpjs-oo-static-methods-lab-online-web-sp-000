946fb82199456a7d0588082dfd51f5570031a364
Azraf Chowdhury <achowdhur@ltu.edu>
2020-09-06 10:44:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ class Formatter {
      return string.replace(/[^A-Za-z0-9- ']+/g, '');
   }
 
-  static titleize(string) {
+  static titleize(string){
+    let words = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let strArr = string.split(" ");
+    let finalString = [];
 
-  }
+    strArr.map(function(word){
+      if(words.includes(word) === false){
+        finalString.push(Formatter.capitalize(word));
+      } else {
+        finalString.push(word);
+      }
+    })
+    return Formatter.capitalize(finalString.join(" "));
+  };
 }
